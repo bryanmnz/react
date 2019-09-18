d5ba34f8cc45b6797b7834b84dec156403254d75
Bryan <bryan_mnz@hotmail.com>
2019-09-17 21:46:59 -0500

    listar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import M from "materialize-css";
+import Employees from "../src/controllers/employees";
+import 'materialize-css/dist/css/materialize.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  componentDidMount() {
+    
+    M.AutoInit();
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col m10 offset-m1">Hola mundo</div>
+        <Employees/>
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+           </p>
+          
+            
+        </header>
+      </div>)
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
