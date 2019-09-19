ce3f496a9956bb90a103c379b3891045b1efd4b7
Bryan Munoz <bryan.munoz.ext@claro.com.co>
2019-09-19 08:42:38 -0500

    Funcionalidad Actualizar

--- src/views/employees.js
+++ src/views/employees.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import M from "materialize-css";
 import Forms from './modal-employees';
+const https = require('https');
 const axios = require('axios');
-
+const agent = new https.Agent({
+    rejectUnauthorized: false
+});
 class listEmployees extends Component {
     constructor() {
         super();
@@ -17,7 +20,7 @@ class listEmployees extends Component {
     }
 
     getUser() {
-        axios.get('http://dummy.restapiexample.com/api/v1/employees')
+        axios.get('http://dummy.restapiexample.com/api/v1/employees', { httpsAgent: agent })
             .then((res) => {
                 this.setState({ data: res.data });
             })
@@ -40,24 +43,26 @@ class listEmployees extends Component {
                             <th>Acciones</th>
                         </tr>
                     </thead>
-                    {this.state.data.map((employee, index) => {
-                        return <tr>
-                            <td>{employee.id}</td>
-                            <td>{employee.employee_name}</td>
-                            <td>{employee.employee_salary}</td>
-                            <td>{employee.employee_age}</td>
-                            <td>
-                                <a className="btn" onClick={this.edit.bind(null, employee.id)} href="#">
-                                    edit
+                    <tbody>
+                        {this.state.data.map((employee, index) => {
+                            return (<tr>
+                                <td>{employee.id}</td>
+                                <td>{employee.employee_name}</td>
+                                <td>{employee.employee_salary}</td>
+                                <td>{employee.employee_age}</td>
+                                <td>
+                                    <a className="btn" onClick={this.edit.bind(null, employee.id)} href="#">
+                                        edit
                             </a>
-                                <a className="btn red" onClick={this.delete.bind(null, employee.id)} data-id={employee.id} href="#">
-                                    Delete
+                                    <a className="btn red" onClick={this.delete.bind(null, employee.id)} data-id={employee.id} href="#">
+                                        Delete
                             </a>
-                            </td>
-                        </tr>
-                    })}
+                                </td>
+                            </tr>)
+                        })}
+                    </tbody>
                 </table>
-                <div class="fixed-action-btn">
+                <div className="fixed-action-btn">
                     <a className="btn-floating btn-large green modal-trigger" href="#modal" onClick={this.create}>
                         +
                     </a>
@@ -72,23 +77,42 @@ class listEmployees extends Component {
 
 
     edit(id) {
-        
-        axios.get(`http://dummy.restapiexample.com/api/v1/employee/${id}`)
-            .then((res) => {                
-                
+        var elem = document.querySelector('.modal');
+        let iName = document.getElementById('name');
+        let iSalary = document.getElementById('salary');
+        let iAge = document.getElementById('age');
+        let iKey = document.getElementById('key');
+        axios.get(`http://dummy.restapiexample.com/api/v1/employee/${id}`, { httpsAgent: agent })
+            .then((res) => {
+                if (res.status === 200) {
+                    iName.value = res.data.employee_name;
+                    iSalary.value = res.data.employee_salary;
+                    iAge.value = res.data.employee_age;
+                    iKey.value = res.data.id;
+                    M.Modal.getInstance(elem).open();
+                    window.action = 1;
+                    document.getElementById('title').innerHTML = 'Editar';
+                } else {
+                    throw new Error("Error al traer la informacion");
+                }
             })
             .catch((err) => {
                 M.toast({ html: err });
             });
 
     }
-
+    create(){
+        window.action = 0;
+        document.getElementById("form").reset();
+        document.getElementById('title').innerHTML = 'Crear';
+    }
     delete(id) {
         axios.delete(`http://dummy.restapiexample.com/api/v1/delete/${id}`, {
 
-        }).then((res) => {
+        }, { httpsAgent: agent }).then((res) => {
             if (res.status === 200) {
                 M.toast({ html: 'Se elimino correctamiente' });
+                window.location.reload();
             } else {
                 M.toast({ html: 'No se pudo eliminar, intente mas tarde' });
             }
@@ -97,10 +121,6 @@ class listEmployees extends Component {
             alert(err)
         });
     }
-    create() {
-    }
-    update() { }
-    getById() { }
 
 }
 
--- src/views/modal-employees.js
+++ src/views/modal-employees.js
@@ -1,59 +1,79 @@
 import React, { Component } from 'react';
 import M from "materialize-css";
 const axios = require('axios');
+const https = require('https');
+const agent = new https.Agent({
+    rejectUnauthorized: false
+});
 class formEmployee extends Component {
     constructor() {
         super();
         this.state = { name: '', age: '', salary: '' }
     }
 
-    handleChange(event) {
-
-        var title = `${event.target.name}`;
-
-        var obj = {};
-        obj[title] = event.target.value
-        console.log(obj)
-        this.setState(obj)
+    handleChange() {
+        let iName = document.getElementById('name');
+        let iSalary = document.getElementById('salary');
+        let iAge = document.getElementById('age');
+        let iKey = document.getElementById('key');
+        return { name: iName.value, age: iAge.value, salary: iSalary.value, key: iKey.value };
     }
 
     render() {
         return (
             <div id="modal" className="modal">
                 <form className="modal-content row" id="form">
+                    <label id='title'></label>
+                    <input id="key" type="hidden" name="key" />
                     <div className="input-field col m6">
-                        <input onChange={this.handleChange.bind(this)} id="name" type="text" class="validate" name="name" />
-                        <label class="active" for="name">Nombre</label>
+                        <input id="name" type="text" className="validate" name="name" />
+                        <label className="active" htmlFor="name">Nombre</label>
                     </div>
                     <div className="input-field col m6">
-                        <input onChange={this.handleChange.bind(this)} id="salary" type="text" class="validate" name="salary" />
-                        <label class="active" for="salary">Salario</label>
+                        <input id="salary" type="text" className="validate" name="salary" />
+                        <label className="active" htmlFor="salary">Salario</label>
                     </div>
                     <div className="input-field col m6">
-                        <input onChange={this.handleChange.bind(this)} id="age" type="text" min="18" class="validate" name="age" />
-                        <label class="active" for="age">Edad</label>
+                        <input id="age" type="text" min="18" className="validate" name="age" />
+                        <label className="active" htmlFor="age">Edad</label>
                     </div>
                 </form>
                 <div className="modal-footer">
                     <a href="#!" className="modal-close waves-effect waves-green btn-flat">Cerrar</a>
-                    <a href="#!" className="waves-effect waves-green btn-flat" onClick={this.save.bind(null, this.state)}>Guardar</a>
+                    <a href="#!" className="waves-effect waves-green btn-flat" onClick={this.save.bind(null, this.handleChange)}>Guardar</a>
                 </div>
             </div>
         )
     }
 
-    save(param) {
-        console.log(param);
-        axios.post(`http://dummy.restapiexample.com/api/v1/create`, param).then((res) => {
-            if (res.status === 200) {
-                M.toast({ html: 'Se Creó correctamiente' });
-            } else {
-                M.toast({ html: 'No se pudo crear, intente mas tarde' });
-            }
+    save(callback) {
+        var param = callback();
+        if (window.action === 1) {
 
-        }).catch((err) => {
-            alert(err)
-        });
+            axios.put(`http://dummy.restapiexample.com/api/v1/update/${param.key}`, param,{ httpsAgent: agent }).then((res) => {
+                if (res.status === 200) {
+                    M.toast({ html: 'Se Actualizo correctamiente' });
+                    window.location.reload();
+                } else {
+                    M.toast({ html: 'No se pudo actualizar, intente mas tarde' });
+                }
+
+            }).catch((err) => {
+                M.toast({ html: err });
+            });
+        } else {
+            axios.post(`http://dummy.restapiexample.com/api/v1/create`, param,{ httpsAgent: agent }).then((res) => {
+                if (res.status === 200) {
+                    M.toast({ html: 'Se Creó correctamiente' });
+                    window.location.reload();
+                } else {
+                    M.toast({ html: 'No se pudo crear, intente mas tarde' });
+                }
+
+            }).catch((err) => {
+                M.toast({ html: err });
+            });
+        }
     }
 }
 
